474e94b6bac95da0a94ee47afcc4ae46467d5d03
Martinel-lab <martinel@et.esiea.fr>
2024-12-21 17:35:44 +0100

    Mise à jour init

--- init.sql
+++ init.sql
@@ -14,3 +14,21 @@ CREATE TABLE IF NOT EXISTS messages (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (iddiscussion) REFERENCES discussions(iddiscussion) ON DELETE CASCADE
 );
+
+INSERT INTO discussions (title) VALUES ('Liste de courses');
+SET @discussion_id = (SELECT iddiscussion FROM discussions WHERE title = 'Liste de courses' LIMIT 1);
+INSERT INTO messages (iddiscussion, content) VALUES 
+(@discussion_id, 'Pain'),
+(@discussion_id, 'Lait'),
+(@discussion_id, 'Biscuits'),
+(@discussion_id, 'Poisson'),
+(@discussion_id, 'Légumes');
+
+INSERT INTO discussions (title) VALUES ('Boucherie');
+SET @discussion_id_boucherie = (SELECT iddiscussion FROM discussions WHERE title = 'Boucherie' LIMIT 1);
+INSERT INTO messages (iddiscussion, content) VALUES 
+(@discussion_id_boucherie, 'Poulet'),
+(@discussion_id_boucherie, 'Bœuf'),
+(@discussion_id_boucherie, 'Agneau'),
+(@discussion_id_boucherie, 'Porc'),
+(@discussion_id_boucherie, 'Saucisses');
